697c8760c0e7d98b6a2d60617431bc1e7be1cf5c
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-03-05 05:14:24 +0100

    added new practice and solution

--- 01-practice.ts
+++ 01-practice.ts
@@ -219,9 +219,71 @@ const binaryGapSolution2 = (num: number): number => {
   return Math.max(...zeros);
 };
 
-console.log(binaryGapSolution2(9), 2);
-console.log(binaryGapSolution2(1041), 5);
-console.log(binaryGapSolution2(529), 4);
-console.log(binaryGapSolution2(20), 1);
-console.log(binaryGapSolution2(15), 0);
-console.log(binaryGapSolution2(32), 0);
+// console.log(binaryGapSolution2(9), 2);
+// console.log(binaryGapSolution2(1041), 5);
+// console.log(binaryGapSolution2(529), 4);
+// console.log(binaryGapSolution2(20), 1);
+// console.log(binaryGapSolution2(15), 0);
+// console.log(binaryGapSolution2(32), 0);
+
+// An array A consisting of N integers is given. Rotation of the array means that each element is shifted right by one index, and the last element of the array is moved to the first place. For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7] (elements are shifted right by one index and 6 is moved to the first place).
+
+// The goal is to rotate array A K times; that is, each element of A will be shifted to the right K times.
+
+// Write a function:
+
+//     class Solution { public int[] solution(int[] A, int K); }
+
+// that, given an array A consisting of N integers and an integer K, returns the array A rotated K times.
+
+// For example, given
+//     A = [3, 8, 9, 7, 6]
+//     K = 3
+
+// the function should return [9, 7, 6, 3, 8]. Three rotations were made:
+//     [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
+//     [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
+//     [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
+
+// For another example, given
+//     A = [0, 0, 0]
+//     K = 1
+
+// the function should return [0, 0, 0]
+
+// Given
+//     A = [1, 2, 3, 4]
+//     K = 4
+
+// the function should return [1, 2, 3, 4]
+
+// Assume that:
+
+//         N and K are integers within the range [0..100];
+//         each element of array A is an integer within the range [−1,000..1,000].
+
+// In your solution, focus on correctness. The performance of your solution will not be the focus of the assessment.
+
+// fn rotateArrSolution
+// takes in an array of numbers and k - number of rotations to be made to arr
+// returns a arr with the values rotated
+// cut the end of the array and add it to the start
+
+const rotateArrSolution = (arr: number[], k: number): number[] => {
+  if (k === 0) return arr;
+
+  const newArr = rotateArr2(arr);
+
+  return rotateArrSolution(newArr, k - 1);
+};
+
+const rotateArr2 = (arr: number[]): number[] => {
+  const pop = arr.pop();
+
+  if (pop) {
+    arr.unshift(pop);
+  }
+
+  return arr;
+};
+console.log(rotateArrSolution([3, 8, 9, 7, 6], 3));
